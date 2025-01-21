daa61c866ddf012d332ea892aab8cf46fa76bca4
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2025-01-21 16:49:58 +0000

    Feat: added new route postUser

---
+++ controllers/UsersController.js
@@ -0,0 +1,26 @@
+import hashPasswd from '../utils/hashpwd';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+export default class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const search = await dbClient.db.collection('users').findOne({ email });
+    if (!search) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashpwd = hashPasswd(password);
+    const addUser = await dbClient.db.collection('users').insertOne({ email, password: hashpwd });
+    const newUser = { id: addUser.ops[0]._id, email: addUser.ops[0].email };
+    return res.status(201).json(newUser);
+  }
+}
