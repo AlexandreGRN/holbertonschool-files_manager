7b3f5336ee99e510190416fc8ccd5e6ebbb9c609
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2025-01-21 16:47:55 +0000

    Feat: initialized npm server with npm install

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
